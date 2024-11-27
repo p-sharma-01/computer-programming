c9d2b02f57160819fce8840b16665573b3b9ccfc
Pushpendra Sharma <pushpendra.sharma_cs.h24@gla.ac.in>
2024-11-27 14:26:55 +0530

    Delete coding-assignment/string directory

--- coding-assignment/string/string_q04.py
+++
@@ -1,8 +0,0 @@
-# Given a string containing multiple words, count the characters in each word and display them.
-def countChars(self,s):
-        k=s.split()
-        l=[]
-        for i in k:
-            l.append(len(i))
-        return l
-            
